4bbf832ab6240b1ebe3e57d4f9e0a70c5f9e82dd
Rvato <6_rubato-myuism_2907@ezweb.ne.jp>
2020-06-03 16:36:02 +0900

    Update 20200602100153_devise_create_users.rb

--- db/migrate/20200602100153_devise_create_users.rb
+++ db/migrate/20200602100153_devise_create_users.rb
@@ -4,9 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,               null: false
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # Users
+      t.string     :nickname,           null: false
+      t.string     :email,              null: false, default: ""
+      t.string     :encrypted_password, null: false, default: ""
+      # Profiles
+      t.string     :last_name           null: false
+      t.string     :first_name          null: false
+      t.string     :last_name_kana      null: false
+      t.string     :first_name_kana     null: false
+      t.integer    :birth_year          null: false
+      t.integer    :birth_month         null: false
+      t.integer    :birth_day           null: false
+      t.references :user_id             null: false, foreign_key: true
 
       ## Recoverable
       t.string   :reset_password_token
