db003b620798df15fb9ec1eccb7fa2c04c0b9bc4
Rvato <6_rubato-myuism_2907@ezweb.ne.jp>
2020-06-11 23:21:27 +0900

    Update 20200602100153_devise_create_users.rb

--- db/migrate/20200602100153_devise_create_users.rb
+++ db/migrate/20200602100153_devise_create_users.rb
@@ -5,17 +5,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       # Users
-      t.string     :nickname,           null: false
-      t.string     :email,              null: false, default: "", unique:true
-      t.string     :encrypted_password, null: false, default: ""
-      # Profiles
+      t.string     :nickname,            null: false
+      t.string     :email,               null: false, default: "", unique:true
+      t.string     :encrypted_password,  null: false, default: ""
       t.string     :last_name,           null: false
       t.string     :first_name,          null: false
       t.string     :last_name_kana,      null: false
       t.string     :first_name_kana,     null: false
-      t.integer    :birth_year,          null: false
-      t.integer    :birth_month,         null: false
-      t.integer    :birth_day,           null: false
+      t.date       :birthday,            null: false
 
       ## Recoverable
       t.string   :reset_password_token
