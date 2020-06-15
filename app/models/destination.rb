fe3aaf7157e0f69899aa7a6255d63a113238be18
miyaosummer <yoshihikon9@gmail.com>
2020-06-15 18:52:12 +0900

    add active_hash association
    productモデル・destinationモデルにactive_hashに関するアソシエーションを追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,6 @@
 class Destination < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
   belongs_to :user
   belongs_to_active_hash :prefecture
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :postage_way
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :product_size
   has_many :product_images, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -6,9 +12,4 @@ class Product < ApplicationRecord
   belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id'
   belongs_to :product_category
   belongs_to :product_brand
-  belongs_to_active_hash :product_condition
-  belongs_to_active_hash :postage_way
-  belongs_to_active_hash :shipping_day
-  belongs_to_active_hash :product_size
-  belongs_to_active_hash :prefecture
 end
