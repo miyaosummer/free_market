c6bd51b379eaa55cb6ae548999b6731e55835131
miyaosummer <yoshihikon9@gmail.com>
2020-06-15 13:58:32 +0900

    fix space
    改行ズレが気持ち悪かったので修正

--- db/migrate/20200615042351_create_comments.rb
+++ db/migrate/20200615042351_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text         :comment,     null: false
-	    t.references   :user,	       null: false, foreign_key: true
-	    t.references   :product,     null: false, foreign_key: true
+      t.references   :user,        null: false, foreign_key: true
+      t.references   :product,     null: false, foreign_key: true
       t.timestamps
     end
   end
