492e8860f76b6e28c5ffd17ab919616e61eee9d1
miyaosummer <yoshihikon9@gmail.com>
2020-06-17 10:05:28 +0900

    change-route-user#index
    userコントローラーのnewアクションをindexアクションに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
-  resources :users ,only: :new
+  resources :users ,only: :index
   resources :logs ,only: :index
   resources :products ,only: [:new, :show] do
     collection do
