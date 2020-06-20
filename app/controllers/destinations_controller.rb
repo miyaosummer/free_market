ad4fd849e8355c678995e19ce2780a7333fe24d0
miyaosummer <yoshihikon9@gmail.com>
2020-06-20 20:32:24 +0900

    add destination-sign-up
    住所登録機能の基幹部分を実装
    ビューや制約については未着手

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,9 +2,12 @@ class DestinationsController < ApplicationController
   
   def new
     @destination = Destination.new
+    # @prefecture = Destination.new
   end
 
   def create
+    destination = Destination.create(create_params)
+    redirect_to root_path
   end
 
   def edit
@@ -13,4 +16,9 @@ class DestinationsController < ApplicationController
   def update
   end
 
+
+  private
+  def create_params
+    params.require(:destination).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id)
+  end
 end
