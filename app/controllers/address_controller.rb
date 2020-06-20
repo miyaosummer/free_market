6543f101f58b48a3cf843aa881faa897c63557eb
miyaosummer <yoshihikon9@gmail.com>
2020-06-20 19:04:29 +0900

    change address-destinations
    addressコントローラーをdestinationsコントローラーに名称変更

--- app/controllers/address_controller.rb
+++
@@ -1,8 +0,0 @@
-class AddressController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-  
-end
---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,16 @@
+class DestinationsController < ApplicationController
+  
+  def new
+    @destination = Destination.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
