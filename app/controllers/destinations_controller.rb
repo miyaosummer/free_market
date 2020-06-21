8e09d2aa0bf6d66481f71a02147dd9a084bdc10d
miyaosummer <yoshihikon9@gmail.com>
2020-06-21 12:50:01 +0900

    Revert "Revert "add destinations-signup view main-bar""
    This reverts commit 13518f1a23c0bfb53e6485abaa82f99f9078f5cc.

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -19,6 +19,6 @@ class DestinationsController < ApplicationController
 
   private
   def create_params
-    params.require(:destination).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id)
+    params.require(:destination).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id)
   end
 end
