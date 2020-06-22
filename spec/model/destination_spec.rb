f02750daefd5336e1d131367ccf7f8a2737a5f53
miyaosummer <yoshihikon9@gmail.com>
2020-06-22 14:48:08 +0900

    add word-count_test-code
    文字数についてのテストコードを追加

--- spec/model/destination_spec.rb
+++ spec/model/destination_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 describe Destination do
   describe '#create' do
+#全項目の条件を満たした時は住所登録ができる
+    # it "全項目の条件を満たした場合は住所登録ができる" do
+    #   destination = build(:destination, user_id:"1")
+    #   expect(destination).to be_valid
+    # end
 
+#文字が入力されているかチェックする
     it "first_nameが空欄の時は住所登録ができない" do
      destination = build(:destination, first_name: "")
      destination.valid?
@@ -51,5 +57,16 @@ describe Destination do
       expect(destination.errors[:block]).to include("can't be blank")
     end
 
+#入力された文字が各項目の条件を満たしているか
+    # it "first_nameが35文字以下の時は住所登録できる" do
+    #   destination = build(:destination, first_name: "アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオ") #35文字
+    #   # destination.valid?
+    #   expect(destination).to be_valid
+    # end
+    it "first_nameが36文字以上の時は住所登録ができない" do
+      destination = build(:destination, first_name: "アイウエオアイウエオアイウエオアイウエオアイウエオアイウエオアイウエオア") #36文字
+      destination.valid?
+      expect(destination.errors[:first_name]).to include("is too long (maximum is 35 characters)")#JavaScriptでエラーメッセージを表示させる際にメッセージを修正する
+    end
   end
 end
