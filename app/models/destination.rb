bab1fd67f81efe222d79010174a217b9c895b3d8
miyaosummer <yoshihikon9@gmail.com>
2020-06-22 14:26:14 +0900

    change destination-model_validation
    destinationモデル内の
    postal_code
    prefecture_idの制約を変更した。

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -15,11 +15,10 @@ class Destination < ApplicationRecord
   validates :last_name,        presence: true, length: { maximum: 35 }
   validates :first_name_kana,  presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
   validates :last_name_kana,   presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'はカタカナで入力して下さい'}
-  validates :postal_code,      presence: true, length: { maximum: 8 }, format: { with: VALID_POSTAL_CODE, message: 'のフォーマットが不適切です' }
-  validates :prefecture_id,    presence: true, numericality: { only_integer: true, less_than: 49 }
+  validates :postal_code,      presence: true, length: { maximum: 8 }
+  validates :prefecture_id,    presence: true, numericality: { less_than: 49 }
   validates :city,             presence: true, length: { maximum: 50 }
   validates :block,            presence: true, length: { maximum: 50 }
   validates :phone_number,     format: { with: /\A\d{10,11}\z/, message: 'の入力が正しくありません'}
-
-
+  
 end
