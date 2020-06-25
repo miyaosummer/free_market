99dc3db2875a2c037abd43b22a09781ccd0200fc
nakauchannel <nakau.tech@gmail.com>
2020-06-25 23:27:52 +0900

    commit-error-check

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,17 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @product.images.new
+    @product.product_images.new
   end
 
   def create
-    Product.create(product_params)
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def show
   end
@@ -34,12 +40,21 @@ private
   end
 
   def product_params
-    params.require(:product).permit(:name,:description,:price,:seller_id,:buyer_id,:product_category_id,:product_condition_id,:postage_way_id,:postage,:shipping_day_id,:product_brand_id,:product_size_id,:prefecture_id)
+    params.require(:product).permit(
+      :name, 
+      :description,
+      :price,
+      # :buyer_id,
+      :product_category_id,
+      :product_condition_id,
+      :postage_way_id,
+      # :postage,
+      :shipping_day_id,
+      # :product_brand_id,
+      # :product_size_id,
+      :prefecture_id,
+      product_images_attributes: [:image, :_destroy, :id]
+      ).merge(seller_id: current_user.id).merge(postage: "送料込み").merge(product_size_id: "1")
+      #---> postageとproduct_size_idはNULL制約が入っており、かつ入力できないためMergeで強制的に入れています
   end
 end
-
-private
-
-  def product_params
-    
-  end
