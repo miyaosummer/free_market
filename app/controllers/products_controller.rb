cfde52b48e6493cb15360f7fe78cc0db7435b9fc
tomo <shineon1717@gmail.com>
2020-06-25 21:14:33 +0900

    ユーザーがログインしているか否かで編集・削除・購入ボタンの表示の場合分け

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,8 +4,16 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
   
   def purchase
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :seller_id, :buyer_id, :product_category_id, 
+    :product_condition_id, :postage_way_id, :postage, :shipping_day_id, :product_brand_id, :product_size_id, :prefecture_id)
   end
 end
