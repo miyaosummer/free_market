f02da1e99fa7f969a1520fa65f7dc611bcfb02ac
miyaosummer <yoshihikon9@gmail.com>
2020-06-25 13:14:01 +0900

    change create_params
    可読性向上のためcreate_paramsをdestination_paramsに変更した

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -7,7 +7,7 @@ class DestinationsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @destination = Destination.new(create_params)
+    @destination = Destination.new(destination_params)
 
     if @destination.save
       redirect_to root_path
@@ -23,11 +23,13 @@ class DestinationsController < ApplicationController
   end
 
   def update
+    @destination = Destination.update(destination_params)
+    redirect_to user_path(current_user.id)
   end
 
 
   private
-  def create_params
+  def destination_params
     params.require(:destination).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id)
   end
 end
