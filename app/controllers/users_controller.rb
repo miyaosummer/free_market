d0067c99b4672cc7417dba6b103b735606af3400
miyaosummer <yoshihikon9@gmail.com>
2020-07-01 14:44:17 +0900

    usercontroller内の不必要な記述を削除
    usercontroller内のnewアクション・createアクションが不必要だったため削除した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,31 +3,9 @@ class UsersController < ApplicationController
   def index
   end
 
-  def new
-    @user = User.find(current_user.id)
-    @destination = Destination.new
-  end
-
-  def create
-    @user = User.find(current_user.id)
-    @destination = Destination.new(destination_params)
-
-    if @destination.save
-      redirect_to purchase_products_path
-    else
-      render action: :new
-    end
-    
-  end
-
   def show
     @user = User.find(params[:id])
     @destination = @user.destination
-  end
-
-  private
-  def destination_params
-    params.require(:destination).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id)
   end
   
 end
