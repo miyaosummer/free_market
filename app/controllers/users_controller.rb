a5e1e9b524b4c8950680943f84473fcf1f7fa743
tomo <shineon1717@gmail.com>
2020-07-02 00:40:00 +0900

    usersコントローラーにlogoutアクションの追加とユーザーがログインしていない状態でマイページに遷移できないように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-    @destination = @user.destination
+    if user_signed_in? && current_user.id ==  params[:id].to_i
+      @user = User.find(params[:id])
+      @destination = @user.destination
+    else
+      redirect_to root_path
+    end
   end
-  
-  
+ 
+  def logout
+  end
 end
