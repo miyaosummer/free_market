6d4818d7ecc37d3bec56e40b49bbfdabdf193ca8
miyaosummer <yoshihikon9@gmail.com>
2020-07-03 22:21:38 +0900

    Ajax実装のためのルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tops#index"
-    resources :users ,only: [:index,:new, :show] do
-      resources :destinations ,only: [:index, :new, :create, :edit, :update]
+
+  resources :users ,only: [:index,:new, :show] do
+    resources :destinations ,only: [:index, :new, :create, :edit, :update]
+  end
+
+  resources :tops, only: [:index, :new] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_header_category_children', defaults: { format: 'json' }
+      get 'get_header_category_grandchildren', defaults: { format: 'json' }
     end
+    member do
+      get 'get_header_category_children', defaults: { format: 'json' }
+      get 'get_header_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+
   resources :logs ,only: :index
   resources :products do
     collection do
