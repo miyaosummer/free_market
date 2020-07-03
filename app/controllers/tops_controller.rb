14d433aba41bedde3c1f964a8eb0e71f59b66a0e
miyaosummer <yoshihikon9@gmail.com>
2020-07-03 22:22:43 +0900

    ancestoryによる情報を取得するための記述をtops_controllerに追記

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,29 @@
 class TopsController < ApplicationController
+
+  before_action :set_header_category_parent, only: :index
+  before_action :get_header_category_children
+  before_action :get_header_category_grandchildren
+
+
   def index
     @products = Product.all
   end
+
+  # 親カテゴリーに紐づく子カテゴリーの配列を取得
+  def get_header_category_children
+    @product_category_children = ProductCategory.find(params[:product_category_parent_name]).children
+  end
+
+  # 子カテゴリーに紐づく孫カテゴリーの配列を取得
+  def get_header_category_grandchildren
+    @product_category_grandchildren = ProductCategory.find("#{params[:product_category_child_id]}").children
+  end
+  
+
+  private
+  # 親カテゴリーの配列を取得
+  def set_header_category_parent  
+    @parents = ProductCategory.where(ancestry: nil)
+  end
+
 end
