85c916c297925907ffb45399e5a76c7ec5a647bd
miyaosummer <yoshihikon9@gmail.com>
2020-07-05 00:33:00 +0900

    headerのカテゴリ情報の取得に関する記述をapplicationcontrollerに移動

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,24 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_header_category_parent
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
     logs_path
   end
+
+    # 親カテゴリーに紐づく子カテゴリーの配列を取得
+    def get_header_category_children
+      @product_category_children = ProductCategory.find(params[:parent_id]).children
+    end
+  
+    # 子カテゴリーに紐づく孫カテゴリーの配列を取得
+    def get_header_category_grandchildren
+      @product_category_grandchildren = ProductCategory.find(params[:child_id]).children
+    end
+    
+
   
   private
 
@@ -18,6 +31,11 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  # 親カテゴリーの配列を取得
+  def set_header_category_parent  
+      @parents = ProductCategory.where(ancestry: nil)
   end
 
   protected
--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,27 +1,8 @@
 class TopsController < ApplicationController
 
-  before_action :set_header_category_parent, only: :index
-
-
   def index
     @products = Product.all
   end
 
-  # 親カテゴリーに紐づく子カテゴリーの配列を取得
-  def get_header_category_children
-    @product_category_children = ProductCategory.find(params[:parent_id]).children
-  end
-
-  # 子カテゴリーに紐づく孫カテゴリーの配列を取得
-  def get_header_category_grandchildren
-    @product_category_grandchildren = ProductCategory.find(params[:child_id]).children
-  end
-  
-
-  private
-  # 親カテゴリーの配列を取得
-  def set_header_category_parent  
-    @parents = ProductCategory.where(ancestry: nil)
-  end
 
 end
