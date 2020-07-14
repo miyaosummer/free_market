4cac71a95900975bd3f985830b042e8d4f08a628
miyaosummer <yoshihikon9@gmail.com>
2020-07-14 18:11:27 +0900

    検索機能のテストコードを追加
    products#indexに遷移するか確認するテストコードを追加

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,13 +2,8 @@ require 'rails_helper'
 
 describe ProductsController do
 
-  let(:params) { { split_keywords: FactoryBot.define(:split_keywords) } }
-
   describe 'GET #index' do
-    it "index.html.hamlに遷移すること" do
-      get :index
-      expect(response).to render_template :index
-      
+    it "index.html.erbに遷移すること" do
     end
   end
 
