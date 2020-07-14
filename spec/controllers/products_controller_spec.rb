19fa1f5ecb0e20460364fc7ed69d2237818731c1
miyaosummer <yoshihikon9@gmail.com>
2020-07-14 16:23:54 +0900

    コントローラーのテストコードを追記

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe ProductsController do
-  let(:products){ FactoryBot.create(:product) }
+
+  let(:params) { { split_keywords: FactoryBot.define(:split_keywords) } }
+
   describe 'GET #index' do
     it "index.html.hamlに遷移すること" do
       get :index
       expect(response).to render_template :index
+      
     end
   end
 
