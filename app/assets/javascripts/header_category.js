bbfefcb5597790ad109511f20cef54c638fa5098
miyaosummer <yoshihikon9@gmail.com>
2020-07-19 17:41:43 +0900

    説明文の位置を移動

--- app/assets/javascripts/header_category.js
+++ app/assets/javascripts/header_category.js
@@ -17,9 +17,12 @@ $(function() {
   //「カテゴリー」テキストにマウスが乗った時の処理
     $(".nav__left__category__title").on("mouseover", function() {
       $('.nav__left__category__title').css('font-weight','bold')
-      $(".selected-parent").removeClass("selected-parent")//背景色をつけるために設定済みのクラスを配置する。まずは初期化をする。
-      $(".child_category").remove();//一度子カテゴリを削除することで「カテゴリー」テキストにマウスが乗るたびに親カテゴリのみ表示されるようにする。
-      $(".grand_child_category").remove();//孫カテゴリも削除する。理由は同上
+      //背景色をつけるために設定済みのクラスを配置する。まずは初期化をする。
+      $(".selected-parent").removeClass("selected-parent")
+      //一度子カテゴリを削除することで「カテゴリー」テキストにマウスが乗るたびに親カテゴリのみ表示されるようにする。
+      $(".child_category").remove();
+      //孫カテゴリも削除する。理由は同上
+      $(".grand_child_category").remove();
     });
     $(".nav__left__category__title").on("mouseout", function() {
       $('.nav__left__category__title').css('font-weight','normal')
@@ -27,37 +30,49 @@ $(function() {
   //
 
     $(".parent_category").on("mouseover", function() {
-      var id = this.id//マウスが乗っているリンクのid番号を取得する
-      $(".selected-parent").removeClass("selected-parent")//背景色をつけるために設定済みのクラスを配置する。まずは初期化をする。
-      $('#' + id).addClass("selected-parent");//選択したカテゴリーに色を付けるクラスを付与する。
-      $(".child_category").remove();//子カテゴリを削除する。これをしないと親カテゴリ内で移動した時に、表示させた子カテゴリが消えず縦に並んでしまう。
-      $(".grand_child_category").remove();//孫カテゴリも削除する。
-      $('.nav__left__category__title').css('font-weight','bold')//「カテゴリ」テキストを太字のままにしておく
+      //マウスが乗っているリンクのid番号を取得する
+      var id = this.id
+      //背景色をつけるために設定済みのクラスを配置する。まずは初期化をする。
+      $(".selected-parent").removeClass("selected-parent")
+      //選択したカテゴリーに色を付けるクラスを付与する。
+      $('#' + id).addClass("selected-parent");
+      //子カテゴリを削除する。これをしないと親カテゴリ内で移動した時に、表示させた子カテゴリが消えず縦に並んでしまう。
+      $(".child_category").remove();
+      //孫カテゴリも削除する。
+      $(".grand_child_category").remove();
+      //「カテゴリ」テキストを太字のままにしておく
+      $('.nav__left__category__title').css('font-weight','bold')
       $.ajax({
         type: 'GET',
         url: '/tops/get_header_category_children',
         data: {parent_id: id},
         dataType: 'json'
       }).done(function(children) {
-        children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
-          var html = buildChildHTML(child);//HTMLにして
-          $(".nav__left__category__list__children").append(html);//リストに追加する
+        //帰ってきた子カテゴリー（配列）
+        children.forEach(function (child) {
+          //HTMLにして
+          var html = buildChildHTML(child);
+          //リストに追加する
+          $(".nav__left__category__list__children").append(html);
         })
       });
     });
 
-    $(".nav__left__category__list").on("mouseover", function() {//「テキスト」の文字を太字にする。カテゴリリストからマウスが離れた時に元の太さに戻す。
+    //「テキスト」の文字を太字にする。カテゴリリストからマウスが離れた時に元の太さに戻す。
+    $(".nav__left__category__list").on("mouseover", function() {
       $('.nav__left__category__title').css('font-weight','bold')
     });
     $(".nav__left__category__list").on("mouseout", function() {
       $('.nav__left__category__title').css('font-weight','normal')
     });
 
-  
-    $(document).on("mouseover", ".child_category", function () {//動的に追加されたHTML
+    //動的に追加されたHTML
+    $(document).on("mouseover", ".child_category", function () {
       var id = this.id
-      $(".selected-child").removeClass("selected-child")//親カテゴリ同様背景色をつけるために設定済みのクラスを配置する。まずは初期化をさせる。
-      $('#' + id).addClass("selected-child");//選択したカテゴリーに色を付けるクラスを付与する。
+      //親カテゴリ同様背景色をつけるために設定済みのクラスを配置する。まずは初期化をさせる。
+      $(".selected-child").removeClass("selected-child")
+      //選択したカテゴリーに色を付けるクラスを付与する。
+      $('#' + id).addClass("selected-child");
       $.ajax({
         type: 'GET',
         url: '/tops/get_header_category_grandchildren',
