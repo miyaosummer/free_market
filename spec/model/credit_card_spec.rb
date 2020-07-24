f3f26d24bbc310ee721b8f763569dde2965abee4
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-07-24 17:28:02 +0900

    バリデーションとテストコードの記述

---
+++ spec/model/credit_card_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe CreditCard do
+  describe '#create' do
+    it '全ての項目を入力すると登録できる' do
+      user = create(:user)
+      card = build(:credit_card, user_id: user.id)
+      card.valid?
+      expect(card).to be_valid
+    end
+
+    it 'customer_idがないと登録できない' do
+      user = create(:user)
+      card = build(:credit_card, user_id: user.id, customer_id: "")
+      card.valid?
+      expect(card.errors[:customer_id]).to include("can't be blank")
+    end
+
+    it 'card_idがないと登録できない' do
+      user = create(:user)
+      card = build(:credit_card, user_id: user.id, card_id: "")
+      card.valid?
+      expect(card.errors[:card_id]).to include("can't be blank")
+    end
+  end
+  
+end
