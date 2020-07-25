125257f1e07bda8202f24786e7d43e1bfa71ce08
YuuKi <6_rubato-myuism_2907@ezweb.ne.jp>
2020-07-26 07:56:42 +0900

    JSファイルの生成

---
+++ app/assets/javascripts/credit_crad.js
@@ -0,0 +1,13 @@
+//---- validates ----//
+$(function(){
+  // エラーメッセージ生成
+  function buildInputError(text) {
+    const html = `
+    <div class="input-error">${text}</div>
+    `;
+    return html;
+  }
+  // エラーチェック関数 ※submitボタンが押されたときの処理
+  function formChecker() {
+  }
+});
