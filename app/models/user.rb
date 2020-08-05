7ef4f80522928f07d47e7a5bc9a4bd22ab3ad098
miyaosummer <yoshihikon9@gmail.com>
2020-08-05 14:07:32 +0900

    ユーザー登録に関するバリデーションを追加
    ・パスワードは7文字以上
    ・名前のカナ入力は全角のみ

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,14 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_KATAKANA_REGEX = /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/
 
   validates :nickname, presence: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
   validates :birthday, presence: true
+  validates :first_name_kana,  presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'は全角カタカナで入力して下さい'}
+  validates :last_name_kana,   presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'は全角カタカナで入力して下さい'}
 
   has_one :destination, dependent: :destroy
   has_one :credit_card, dependent: :destroy
